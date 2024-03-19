001adf0ad0792a0f845963315c55fd7408379de4
Nyakamba <omwegaenock@gmail.com>
2024-03-19 08:35:18 +0300

    Projects

---
+++ portfolio/src/components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {} from "../data";
+
+const Projects = () => {
+  return (
+    <div>
+      <div className="w-full flex flex-wrap items-center justify-center gap-10 lg:gap-20 pt-10">
+        {}
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
